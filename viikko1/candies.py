6f734a58c662fa4f5010223305ed10373be20c04
Zheniitti <liao.zhenyu.zl@gmail.com>
2024-01-31 13:29:22 +0200

    Create candies.py

---
+++ viikko1/candies.py
@@ -0,0 +1,20 @@
+""" 
+Nallekarkki maksaa a euroa ja suklaakarkki maksaa b euroa. Montako karkkia voit ostaa enintään, jos sinulla on yhteensä rahaa c euroa?
+Voit olettaa, että a, b ja c ovat kokonaislukuja välillä 1 - 100.
+Toteuta tiedostoon candies.py funktio count, joka antaa suurimman karkkien määrän.
+ """
+
+def count(a, b, c):
+    if a < b:
+        return c // a
+    else :
+        return c // b
+
+if __name__ == "__main__":
+    print(count(3, 4, 11)) # 3
+    print(count(5, 1, 100)) # 100
+    print(count(2, 3, 1)) # 0
+    print(count(2, 3, 9)) # 4
+
+  git config --global user.email "liao.zhenyu.zl@gmail.com"
+  git config --global user.name "Zheniitti"
